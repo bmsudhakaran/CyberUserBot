c88c5d9ff362d6bae53b6b7f23459a94645b69ec
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-02 23:13:50 +0400

    ㅤ

--- userbot/modules/tag.py
+++ userbot/modules/tag.py
@@ -33,7 +33,7 @@ async def _(q):
 			dayandir=False
 			msjcgr = None
 			break
-		if a_ == allLimit:
+		if a_ == taglimit:
 			dayandir=False
 			msjcgr = None
 			break
@@ -66,7 +66,7 @@ async def _(q):
 			dayandir=False
 			msjcgr = None
 			break
-		if a_ == allLimit:
+		if a_ == taglimit:
 			dayandir=False
 			msjcgr = None
 			break
