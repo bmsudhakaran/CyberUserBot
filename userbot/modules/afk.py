c44b78fd3e43106ff6c925631c9cccf10ae85a71
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-09 19:03:13 +0400

    C Y B Ξ R: afk update

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -10,7 +10,7 @@ from asyncio import sleep
 from telethon.events import StopPropagation
 
 from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,
-                     BOTLOG_CHATID, USERS, PM_AUTO_BAN, SON_GORULME)
+                     BOTLOG_CHATID, USERS, PM_AUTO_BAN, SON_GORULME, StartTime)
 from userbot.events import register
 from userbot.main import PLUGIN_MESAJLAR
 from time import time
@@ -35,6 +35,31 @@ def time_formatter(seconds, short=True):
         ((str(minutes) + (" dəqiqə, " if not short else "d, ")) if minutes else "") + \
         ((str(seconds) + (" saniyə, " if not short else "s, ")) if seconds else "")
     return tmp[:-2] + " əvvəl"
+ 
+
+async def noafktime(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["saniyə", "dəqiqə", "saat", "gün"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ", ".join(time_list)
+
+    return up_time
   
 
 @register(incoming=True, disable_errors=True, disable_edited=True)
@@ -321,7 +346,6 @@ async def asistanafk(ups):
     
 @register(outgoing=True)
 async def type_afk_is_not_true(notafk):
-    """ Bu kısım bir yere bir şey yazdığınızda sizi AFK modundan çıkarmaya yarar. """
     global ISAFK
     global COUNT_MSG
     global USERS
