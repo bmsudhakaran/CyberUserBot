f84d7fcd7a0c9aa105b9a43e3c0538df141fa6d1
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-11 16:00:18 +0400

    ㅤ

--- userbot/modules/clive.py
+++ userbot/modules/clive.py
@@ -1,3 +1,5 @@
+# CYBERUSERBOT - FARIDDADASHZADE
+
 import asyncio
 from telethon import events
 from userbot import SUDO_ID, SUDO_VERSION, CYBER_VERSION
@@ -9,5 +11,5 @@ async def sudoers(s):
     await s.client.send_message(s.chat_id,f"`C Y B Ξ R USERBOT\nSudo aktivdir...\nC Y B Ξ R Version: {CYBER_VERSION}\nSudo Version: {SUDO_VERSION}`")
     
 Help = CmdHelp('calive')
-Help.add_command('calive',   None, 'Sudo aktiv olub olmadığını yoxlamaq üçün.')
+Help.add_command('calive', None, 'Sudo aktiv olub olmadığını yoxlamaq üçün.')
 Help.add()
