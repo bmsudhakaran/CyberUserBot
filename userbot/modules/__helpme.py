7cb86fda8156e347c897ca70545046815a5c8e5c
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-11 15:23:47 +0400

    ㅤ

--- userbot/modules/__helpme.py
+++ userbot/modules/__helpme.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# TheCyberUserBot - Luciferxz
+# CYBERUSERBOT - FARIDDADASHZADE
 
 # @Qulec tarafından yazılmıştır.
 # Thanks @Spechide.
