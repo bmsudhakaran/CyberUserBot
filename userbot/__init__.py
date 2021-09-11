64436a04a5960a9314e8a1e3d768e84aee82d844
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-11 22:47:17 +0400

    C Y B Ξ R: init make it better

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -71,7 +71,7 @@ API_KEY = os.environ.get("API_KEY", None)
 API_HASH = os.environ.get("API_HASH", None)
 
 try:
-    SUDO_ID = set(int(x) for x in os.environ.get("SUDO_ID", "").split())
+    SUDO_ID = {int(x) for x in os.environ.get("SUDO_ID", "").split()}
 except ValueError:
     raise Exception("SUDO_ID qeyd etməmisiniz!")
     
