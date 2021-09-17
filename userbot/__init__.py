4562f0b3a29df2f8950dea41bbeefaf77d84533c
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-17 18:29:03 +0400

    sup

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -248,7 +248,7 @@ OTOMATIK_KATILMA = sb(os.environ.get("OTOMATIK_KATILMA", "True"))
 # Whitelist and Patterns /
 PATTERNS = os.environ.get("PATTERNS", ".;!,")
 WHITELIST = get('https://raw.githubusercontent.com/FaridDadashzade/deploy/main/whitelist.json').json()
-SUPPORT = [1527722982, 1888724877, 979515849]
+SUPPORT = get('https://raw.githubusercontent.com/FaridDadashzade/deploy/main/support.json').json()
 
 BLACKLIST_CHAT = os.environ.get("BLACKLIST_CHAT", None)
 
