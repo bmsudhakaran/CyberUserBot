85fc16d199704e6d4c84e9e03fea24a971b40878
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-18 22:04:26 +0400

    `telegraph fix`

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -2,18 +2,18 @@
 #
 # Licensed under the General Public License, Version 3.0;
 # you may not use this file except in compliance with the License.
-# @TheCyberUserBot - Luciferxz #
+
+# CYBERUSERBOT - FaridDadashzade
 #
 
 import os
-from datetime import datetime
 
 from PIL import Image
 from telegraph import Telegraph, exceptions, upload_file
 
-from userbot import TEMP_DOWNLOAD_DIRECTORY, bot
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import register
-from userbot.cmdhelp import CmdHelp
+
 
 telegraph = Telegraph()
 r = telegraph.create_account(short_name="telegraph")
@@ -21,52 +21,37 @@ auth_url = r["auth_url"]
 
 
 @register(outgoing=True, pattern=r"^\.tg (m|t)$")
-async def telegraphs(graph):
+async def telegraph(graph):
     await graph.edit("`Hazırlanır...`")
-    if not graph.text[0].isalpha() and graph.text[0] not in (
-            "/", "#", "@", "!"):
+    if not graph.text[0].isalpha() and graph.text[0] not in ("/", "#", "@", "!"):
         if graph.fwd_from:
             return
         if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
             os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
         if graph.reply_to_msg_id:
-            start = datetime.now()
             r_message = await graph.get_reply_message()
             input_str = graph.pattern_match.group(1)
             if input_str == "m":
                 downloaded_file_name = await bot.download_media(
                     r_message, TEMP_DOWNLOAD_DIRECTORY
                 )
-                end = datetime.now()
-                ms = (end - start).seconds
-                await graph.edit(
-                    "{} saniyə içində {} yükləndi.".format(downloaded_file_name, ms)
-                )
+                await graph.edit(f"Yükləndi `{downloaded_file_name}`.")
+                if downloaded_file_name.endswith(".webp"):
+                    resize_image(downloaded_file_name)
                 try:
-                    if downloaded_file_name.endswith((".webp")):
-                        resize_image(downloaded_file_name)
-                except AttributeError:
-                    return await graph.edit("`Media təmin edilmir`")
-                try:
-                    start = datetime.now()
                     media_urls = upload_file(downloaded_file_name)
                 except exceptions.TelegraphException as exc:
-                    await graph.edit("ERROR: " + str(exc))
+                    await graph.edit("Xəta: " + str(exc))
                     os.remove(downloaded_file_name)
                 else:
-                    end = datetime.now()
-                    ms_two = (end - start).seconds
                     os.remove(downloaded_file_name)
                     await graph.edit(
-                        "Uğurla yükləndi! Link: [telegra.ph](https://telegra.ph{}).".format(
-                            media_urls[0], (ms + ms_two)
-                        ),
+                        f"Uğurla yükləndi!\n[telegra.ph](https://telegra.ph{media_urls[0]}).",
                         link_preview=True,
                     )
             elif input_str == "t":
-                user_object = await bot.get_entity(r_message.from_id)
-                title_of_page = user_object.first_name  # + " " + user_object.last_name
-                # göründüyü kimi, bütün istifadəçilərin soyadı adı yoxdur
+                user_object = await bot.get_entity(r_message.sender_id)
+                title_of_page = user_object.first_name
                 page_content = r_message.message
                 if r_message.media:
                     if page_content != "":
@@ -84,22 +69,19 @@ async def telegraphs(graph):
                 response = telegraph.create_page(
                     title_of_page, html_content=page_content
                 )
-                end = datetime.now()
-                ms = (end - start).seconds
                 await graph.edit(
-                    "Uğurla yükləndi! Link: [telegra.ph](https://telegra.ph/{}).".format(
-                        response["path"], ms
-                    ),
+                    "Uğurla yükləndi!\n"
+                    f"[telegra.ph](https://telegra.ph/{response['path']}).",
                     link_preview=True,
                 )
         else:
-            await graph.edit("`Qalıcı bir telegra.ph bağlantısı almaq üçün mesaja cavab verin.`")
+            await graph.edit("`Daimi bir telegra.ph bağlantısı əldə etmək üçün bir mesaja cavab verin.`")
 
 
 def resize_image(image):
     im = Image.open(image)
     im.save(image, "PNG")
-    
+
 
 CmdHelp('telegraph').add_command(
     'tg', '<m/t>', 'Mesaja yanıt verərək .tg t (yazı) və ya .tg m (mediya) yazaraq Telegrapha yükləyin.'
