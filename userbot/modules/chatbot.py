6a230a494d820dc672294fd20151aa27d7b5fadd
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-19 11:54:49 +0400

    added `chatbot` module

---
+++ userbot/modules/chatbot.py
@@ -0,0 +1,85 @@
+# Copyright (C) 2021 FaridDadashzade.
+#
+# Licensed under MIT license;
+# you may not use this file except in compliance with the License.
+
+# All rights reserved.
+
+import requests
+from googletrans import Translator
+from telethon import events
+from telethon.tl.types import User
+
+from userbot import BLACKLIST_CHAT
+from userbot import LOGS, bot
+from userbot.events import register
+from userbot.cmdhelp import CmdHelp
+
+translator = Translator()
+LANGUAGE = "az"
+
+aktivet = []
+
+url = "https://api-tede.herokuapp.com/api/chatbot?message={message}"
+
+
+# ---------------------------------- #
+
+from userbot.language import get_value
+LANG = get_value("cyberlangs")
+
+# ---------------------------------- #
+
+async def cavablama(message):
+    link = url.format(message=message)
+    try:
+        data = requests.get(link)
+        if data.status_code == 200:
+            return (data.json())["msg"]
+        else:
+            LOGS.info("Xəta baş verdi!")
+    except Exception:
+        LOGS.info("Xəta: {str(e)}")
+
+
+async def aktivetme(db, event):
+    if event.chat_id in BLACKLIST_CHAT:
+        return await event.edit(LANG["PROHIBITED_COMMAND"])
+    status = event.pattern_match.group(1).lower()
+    chat_id = event.chat_id
+    if status == "on":
+        if chat_id not in db:
+            db.append(chat_id)
+            return await event.edit("Chatbot aktivdir!")
+        await event.edit("Chatbot deaktivdir.")
+    elif status == "off":
+        if chat_id in db:
+            db.remove(chat_id)
+            return await event.edit("Chatbot deaktivdir!")
+        await event.edit("Chatbot deaktivdir..")
+    else:
+        await event.edit("**Istifadəsi:**\n.chatbot <on/off>")
+
+
+@register(outgoing=True, pattern=r"^\.chatbot(?: |$)(.*)")
+async def chatbot(event):
+    await aktivetme(aktivet, event)
+
+
+@register(incoming=True)
+async def chatbot(event):
+    sender = await event.get_sender()
+    if not isinstance(sender, User):
+        return
+    if event.chat_id not in aktivet:
+        return
+    if event.text and event.is_reply:
+        rep = await cavablama(event.message.message)
+        tr = translator.translate(rep, LANGUAGE)
+        if tr:
+            await event.reply(tr.text)
+            
+Help = CmdHelp('chatbot')
+Help.add_command('chatbot', '<on>', 'Chatbot modulunu aktiv edər.')
+Help.add_command('chatbot', '<off>', 'Chatbot modulunu deaktiv edər.')
+Help.add()
