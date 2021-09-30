5440d138ef24db6562ebc65f2904bcf6e8bef618
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-30 14:40:48 +0400

    Update clive.py

--- userbot/modules/clive.py
+++ userbot/modules/clive.py
@@ -6,7 +6,7 @@ from userbot import SUDO_ID, SUDO_VERSION, CYBER_VERSION
 from userbot.cmdhelp import CmdHelp
 from userbot.events import register
 
-@register(incoming=True, from_users=SUDO_ID, pattern="^.calive$")
+@register(sudo=True, pattern="^.calive$")
 async def sudoers(s):
     await s.client.send_message(s.chat_id,f"`C Y B Ξ R USERBOT\nSudo aktivdir...\nC Y B Ξ R Version: {CYBER_VERSION}\nSudo Version: {SUDO_VERSION}`")
     
