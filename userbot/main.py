4a34412020b1fcb457ff81edc53ce86dcf573553
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-10-14 11:03:29 +0400

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -290,9 +290,9 @@ async def get_readable_time(seconds: int) -> str:
 async def startupcyber():
     try:
         islememuddeti = await get_readable_time((time.time() - StartTime))
-        await bot.client.send_message(QRUP,f"Salam! mən C Y B Ξ R UserBot\nBotumuzu qurduğunuz üçün təşəkkür edirəm!\nBotunuz aktivdir.\n\nC Y B Ξ R: **{CYBER_VERSION}**\nİşləmə müddəti: **{islememuddeti}**\n\nYardıma ehtiyyacınız olarsa @TheCyberSupport qrupuna yazın :)")
+        await bot.send_file(QRUP, LOGO_STR, caption=f"Salam! mən C Y B Ξ R UserBot\nBotumuzu qurduğunuz üçün təşəkkür edirəm!\nBotunuz aktivdir.\n\nC Y B Ξ R: **{CYBER_VERSION}**\nİşləmə müddəti: **{islememuddeti}**\n\nYardıma ehtiyyacınız olarsa @TheCyberSupport qrupuna yazın :)")
     except:
-        return False
+    return False
     
 
 async def FotoDegistir (foto):
