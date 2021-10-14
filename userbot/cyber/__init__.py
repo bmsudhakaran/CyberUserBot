6311e6e6b60e605972305faf27d47f783052b73a
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-10-14 13:06:13 +0400

    C Y B Ξ R

--- userbot/cyber/__init__.py
+++ userbot/cyber/__init__.py
@@ -1,1 +1,22 @@
-#
+# Copyright 2021 (C) FaridDadashzade.
+
+from telethon import Button, custom
+from userbot import bot, DEFAULT_NAME, SAHIB_ID, SUPPORT
+from userbot import *
+from . import *
+
+
+CYBER_NAME = f"[{DEFAULT_NAME}](tg://user?id={SAHIB_ID})"
+
+SAHIB = SUPPORT
+
+async def qur(cyber, ad, deyer):
+    try:
+        cyber.set(ad, deyer)
+    except BaseException:
+        return await cyber.edit("`Xəta.`")
+      
+      
+def geri(ad):
+    button = [Button.inline("« Geri", data=f"{ad}")]
+    return button
