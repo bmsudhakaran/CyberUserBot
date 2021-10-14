872b6d10ae6c261c771594723a4311d24bdb0e68
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-10-14 16:22:44 +0400

    `C Y B Ξ R: main`

--- userbot/main.py
+++ userbot/main.py
@@ -34,7 +34,7 @@ from time import time
 import userbot.events
 
 CYBER_NAME = f"[{DEFAULT_NAME}](tg://user?id={SAHIB_ID})"
-QRUP = "me"
+QRUP = BOTLOG_CHATID
 
 def cyber_time(seconds, short=True):
     minutes, seconds = divmod(int(seconds), 60)
@@ -290,9 +290,9 @@ async def get_readable_time(seconds: int) -> str:
 
 async def startupcyber():
     try:
-        file = ["https://telegra.ph/file/c3e75eccaeb7f56dfae89.mp4"]
+        file = "https://telegra.ph/file/c3e75eccaeb7f56dfae89.mp4"
         islememuddeti = await get_readable_time((time.time() - StartTime))
-        await bot.send_file(QRUP, file, caption=f"Salam! mən C Y B Ξ R UserBot\nBotumuzu qurduğunuz üçün təşəkkür edirəm!\nBotunuz aktivdir.\n\nC Y B Ξ R: **{CYBER_VERSION}**\nİşləmə müddəti: **{islememuddeti}**\n\nYardıma ehtiyyacınız olarsa @TheCyberSupport qrupuna yazın :)")
+        await bot.client.send_file(BOTLOG_CHATID, file,caption=f"Salam! mən C Y B Ξ R UserBot\nBotumuzu qurduğunuz üçün təşəkkür edirəm!\nBotunuz aktivdir.\n\nC Y B Ξ R: **{CYBER_VERSION}**\nİşləmə müddəti: **{islememuddeti}**\n\nYardıma ehtiyyacınız olarsa @TheCyberSupport qrupuna yazın :)", force_document=False)
     except:
         return False
     
